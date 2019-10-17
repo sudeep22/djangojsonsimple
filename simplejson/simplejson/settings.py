54a95753fe6555c084ed7247413e3b32d13267af
sudeep22 <39305973+sudeep22@users.noreply.github.com>
2019-10-17 16:15:04 +0530

    Update settings.py

--- simplejson/simplejson/settings.py
+++ simplejson/simplejson/settings.py
@@ -32,9 +32,9 @@ ALLOWED_HOSTS = []
 
 # Application definition
 
-INSTALLED_APPS = [    
-    'django.contrib.auth',
+INSTALLED_APPS = [
     'django.contrib.admin',
+    'django.contrib.auth',    
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
